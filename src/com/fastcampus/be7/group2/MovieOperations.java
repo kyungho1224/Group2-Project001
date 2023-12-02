50448bfcd3e2938ab5bb43298a9ee6edde951f1c
kimkyungho <dev_kimkyungho@kakao.com>
2023-12-03 00:18:07 +0900

    초기화면구성, 영화입력, 영화출력 구현(상세는 README)

--- src/com/fastcampus/be7/group2/MovieOperations.java
+++ src/com/fastcampus/be7/group2/MovieOperations.java
@@ -1,9 +1,95 @@
 package com.fastcampus.be7.group2;
 
+import com.fastcampus.be7.group2.model.MovieDTO;
+
+import java.util.Scanner;
+
 /**
  * Created by KimKyungHo on 2023-12-01(001)
  */
 
 /* 메인 클래스 */
 public class MovieOperations {
+
+    public static void main(String[] args) {
+
+        // 기능 구현 클래스
+        MovieProcessor movieProcessor = new MovieProcessor();
+
+        while (true) {
+
+            System.out.println("========== 장르별 영화 검색 프로그램(2조-잠은 죽어서 자야조) ==========");
+            System.out.println("1.영화입력(I)\t2.영화출력(P)\t3.영화검색(S)\t4.종료(E)");
+            System.out.println("===============================================================");
+            System.out.print("메뉴입력 : ");
+
+            // 사용자 입력을 받을 스캐너
+            Scanner scanner = new Scanner(System.in);
+            String menu = scanner.nextLine();
+
+            if (menu.equalsIgnoreCase("I")) {
+                // TODO 영화입력
+
+                if (movieProcessor.getMaxMovieCount() == 0) {
+                    // - 먼저, 영화 데이터의 수를 입력하고
+                    System.out.print("영화 데이터의 수를 입력해주세요 : ");
+                    movieProcessor.initMovie(scanner.nextInt());
+                    scanner.nextLine();
+                }
+
+                // MovieDTO 배열을 사용하여 각 영화의 세부 사항을 저장합니다.
+                System.out.print("제목을 입력해주세요 : ");
+                String title = scanner.nextLine();
+                System.out.print("주인공을 입력해주세요 : ");
+                String major = scanner.nextLine();
+                System.out.print("상영시간을 입력해주세요 : ");
+                int runningTime = scanner.nextInt();
+                scanner.nextLine();
+                System.out.print("평점을 입력해주세요 : ");
+                float rating = scanner.nextFloat();
+                scanner.nextLine();
+                System.out.print("장르를 입력해주세요 : ");
+                int genre = scanner.nextInt();
+
+                // 최초에 지정한 영화 데이터의 수를 넘어가면 배열을 늘린다.
+                if (movieProcessor.getMaxMovieCount() == movieProcessor.getCurrentMovieCount()) {
+                    movieProcessor.increaseMaxCount();
+                }
+                // MovieDTO 저장
+                MovieDTO movieDTO = new MovieDTO(title, major, runningTime, rating, genre);
+                movieProcessor.addMovie(movieDTO);
+                System.out.println("영화 저장이 완료되었습니다.");
+                System.out.println("총 " + movieProcessor.getCurrentMovieCount() + "개의 영화가 저장되어 있습니다.");
+
+            } else if (menu.equalsIgnoreCase("P")) {
+                // TODO 영화출력
+
+                // 저장된 영화가 없을 때 영화출력을 요청했을 때 처리
+                if (movieProcessor.getCurrentMovieCount() > 0) {
+                    MovieDTO[] movies = movieProcessor.getMovieList();
+                    for (MovieDTO movie : movies) {
+                        if (movie != null) {
+                            System.out.println(movie);
+                        }
+                    }
+                } else {
+                    System.out.println("저장된 영화가 없습니다!");
+                }
+            } else if (menu.equalsIgnoreCase("S")) {
+                // TODO 영화검색
+            } else if (menu.equalsIgnoreCase("E")) {
+                // TODO 종료
+                break;
+            } else if (menu.equals("-1")) {
+                break;
+            } else {
+                // TODO 메뉴에 없는 입력을 받았을 때 오류 처리
+                // 스캐너에서 nextInt()를 받기 때문에 문자열을 입력했을때는 다르게 처리가 필요
+                System.out.println("잘못된 입력입니다. 메뉴를 다시 확인하고 입력해주세요.");
+            }
+
+        }
+
+    }
+
 }
--- src/com/fastcampus/be7/group2/MovieProcessor.java
+++ src/com/fastcampus/be7/group2/MovieProcessor.java
@@ -1,12 +1,72 @@
 package com.fastcampus.be7.group2;
 
+import com.fastcampus.be7.group2.model.MovieDTO;
+
 /**
  * Created by KimKyungHo on 2023-12-01(001)
  */
 
 /* 기능 구현 클래스 */
 public class MovieProcessor {
-    public static void main(String[] args) {
 
+    private MovieDTO[] movies;
+    private int maxMovieCount = 0;  // 저장할 영화의 최대 개수
+    private int currentMovieCount = 0;  // 현재 저장된 영화 개수
+
+    public MovieProcessor() {
     }
+
+    // 영화의 데이터 수를 입력하고...라는 명세서 내용때문에 초기화하는 부분
+    public void initMovie(int movieCount) {
+        // - 먼저, 영화 데이터의 수를 입력하고
+        maxMovieCount = movieCount;
+        movies = new MovieDTO[movieCount];
+    }
+
+    public int getMaxMovieCount() {
+        return maxMovieCount;
+    }
+
+    public void increaseMaxCount() {
+        maxMovieCount++;
+        MovieDTO[] afterMovies = new MovieDTO[maxMovieCount];
+        for (int i = 0; i < movies.length; i++) {
+            afterMovies[i] = movies[i];
+        }
+        movies = afterMovies;
+    }
+
+    public int getCurrentMovieCount() {
+        return currentMovieCount;
+    }
+
+    public void addMovie(MovieDTO movie) {
+        movies[currentMovieCount] = movie;
+        currentMovieCount++;
+    }
+
+    /**
+     * 평점을 기준으로 내림차순 정렬하여 영화목록을 출력
+     **/
+    public MovieDTO[] getMovieList() {
+        if (currentMovieCount > 1) {
+            return sortMovieList();
+        }
+        return movies;
+    }
+
+    public MovieDTO[] sortMovieList() {
+        for (int i = 0; i < currentMovieCount; i++) {
+            for (int j = i + 1; j < currentMovieCount; j++) {
+                if (movies[i].getRating() < movies[j].getRating()) {
+                    MovieDTO tmpMovie;
+                    tmpMovie = movies[i];
+                    movies[i] = movies[j];
+                    movies[j] = tmpMovie;
+                }
+            }
+        }
+        return movies;
+    }
+
 }
